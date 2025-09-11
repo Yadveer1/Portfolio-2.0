f191eb109107c76241d25e595cade9c5a2822b50
Yadveer1 <134469422+Yadveer1@users.noreply.github.com>
2025-09-11 23:50:15 +0530

    Update base path in Vite configuration for local development

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio-2.0/',
+  base: './',
   build: {
     rollupOptions: {
       output: {
